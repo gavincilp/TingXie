af89b6e59f105dd43c959a5f3f0dfac835bed9d5
xingjf <xinggavin@foxmail.com>
2022-11-02 10:33:47 +0800

    处理轻声问题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-   <Spin size="large" fix :show="spinShow"></Spin>
+   
    <a href="https://github.com/gavincilp/TingXie" class="github-corner" aria-label="View source on GitHub"><svg width="100" height="100" viewBox="0 0 250 250" style="z-index:200;fill:#8bc34a; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>
+   <Spin size="large" fix :show="spinShow"></Spin>
    <Layout >
         <Header class="layout-header">
             <img src="./assets/monkey.svg" class="layout-logo"/>
@@ -306,9 +307,19 @@ export default {
                 if(pinyinArr.length==1){
                     o.push({letter:word.word[i],pinyin:pinyin(word.word[i])});
                 }else{
-                    o.push({letter:word.word[i],pinyin:pinyin(word.word[i],{type:'array',multiple:true})[pinyinArr.findIndex(
-                        e=>e.replace('0','').replace('ü','v')==word.pinyin[i]
-                    )]});    
+                    const index = pinyinArr.findIndex(e=>e.replace('0','').replace('ü','v')==word.pinyin[i]);
+                    if(index != -1){
+                        o.push({letter:word.word[i],pinyin:pinyin(word.word[i],{type:'array',multiple:true})[index]});
+                    }else{
+                        const tempArr = pinyin(word.word[i],{ toneType: 'none', type: 'array',multiple:true });
+                        const tempIndex = tempArr.findIndex(e=>e.replace('0','').replace('ü','v').replaceAll(/[0-4]/g,'')==word.pinyin[i]);
+                        if(tempIndex != -1){
+                            o.push({letter:word.word[i],pinyin:tempArr[tempIndex]});
+                        }else{
+                            o.push({letter:word.word[i],pinyin:' '});
+                        }
+                    }
+                    
                 }
                                
             }
@@ -318,8 +329,9 @@ export default {
             this.formItem.total++;
         },
         generate(){
+            this.spinShow=true;
             this.$refs['form'].validate((valid) => {
-                this.spinShow=true;
+                
                 if (valid) {
                     const tempObject={};
                     ciku.filter(e=>{
@@ -358,19 +370,31 @@ export default {
                         tempArray = copy;
                         while(copy.length > 0 && printed.length < this.formItem.total){
                             const currentLetter = copy.shift();
-                            const currentWord = currentLetter.arr.shift();
+                            const word = currentLetter.arr.shift();
                             const o = [];
-                            for(let i = 0 ; i < currentWord.word.length; i ++){
-                                const pinyinArr=pinyin(currentWord.word[i],{ toneType: 'num', type: 'array',multiple:true });
+                            
+                            for(let i = 0 ; i < word.word.length; i ++){
+                                const pinyinArr=pinyin(word.word[i],{ toneType: 'num', type: 'array',multiple:true });
                                 if(pinyinArr.length==1){
-                                    o.push({letter:currentWord.word[i],pinyin:pinyin(currentWord.word[i])});
+                                    o.push({letter:word.word[i],pinyin:pinyin(word.word[i])});
                                 }else{
-                                    o.push({letter:currentWord.word[i],pinyin:pinyin(currentWord.word[i],{type:'array',multiple:true})[pinyinArr.findIndex(e=>e.replace('0','').replace('ü','v')==currentWord.pinyin[i])]});    
+                                    const index = pinyinArr.findIndex(e=>e.replace('0','').replace('ü','v')==word.pinyin[i]);
+                                    if(index != -1){
+                                        o.push({letter:word.word[i],pinyin:pinyin(word.word[i],{type:'array',multiple:true})[index]});
+                                    }else{
+                                        const tempArr = pinyin(word.word[i],{ toneType: 'none', type: 'array',multiple:true });
+                                        const tempIndex = tempArr.findIndex(e=>e.replace('0','').replace('ü','v').replaceAll(/[0-4]/g,'')==word.pinyin[i]);
+                                        if(tempIndex != -1){
+                                            o.push({letter:word.word[i],pinyin:tempArr[tempIndex]});
+                                        }else{
+                                            o.push({letter:word.word[i],pinyin:' '});
+                                        }
+                                    }
+                                    
                                 }
-                               
                             }
-                            currentWord.print=o;
-                            printed.push(currentWord);
+                            word.print=o;
+                            printed.push(word);
                             if(currentLetter.arr.length>0){
                                 tempArray.push(currentLetter);
                             }else{
@@ -380,8 +404,9 @@ export default {
                     }
                     this.printArray=printed;
                     this.currentCiku.push(...tempArray);
-                    this.spinShow=false;
+                    
                 }
+                this.spinShow=false;
             });
         },
         print(){      
